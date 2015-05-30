2838afd10547fb4c2b055715d4952e3c2dd069b1
Joseph Orbegoso Pea <joe@trusktr.io>
2015-05-29 22:58:20 -0700

    Webpack v1.9.10

---
+++ package.js
@@ -0,0 +1,23 @@
+Package.describe({
+  name: 'rocket:webpack',
+  version: '1.9.10',
+  summary: 'Provides webpack from npm.',
+  git: 'git@github.com:trusktr/rocket-webpack.git',
+  documentation: 'README.md'
+})
+
+Npm.depends({
+    'webpack': "1.9.10",
+})
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.1.0.2')
+  api.addFiles('webpack.js', 'server')
+  api.export('Webpack', 'server')
+})
+
+Package.onTest(function(api) {
+  api.use('tinytest')
+  api.use('rocket:webpack')
+  api.addFiles('webpack-tests.js')
+})
---
+++ webpack.js
@@ -0,0 +1,1 @@
+Webpack = Npm.require('webpack')
