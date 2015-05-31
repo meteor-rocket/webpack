c35ac3c8ede2183e6987becbaa2bcd00afb6efff
Joseph Orbegoso Pea <joe@trusktr.io>
2015-05-31 13:40:18 -0700

    Added the babel, css, and style loaders.

--- package.js
+++ package.js
@@ -8,6 +8,14 @@ Package.describe({
 
 Npm.depends({
     'webpack': "1.9.10",
+
+    // packages like rocket:module have to discover the location of the
+    // rocket:webpack isopack in the filesystem and tell Webpack to look in the
+    // isopack's `npm/node_modules` so that Webpack will be able to find the
+    // loaders.
+    'babel-loader': '5.0.0',
+    'css-loader': '0.12.0',
+    'style-loader': '0.12.1'
 })
 
 Package.onUse(function(api) {
