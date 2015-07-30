2e66cfaf4daa3b969d780343e4b8c765438c183d
Joseph Orbegoso Pea <joe@trusktr.io>
2015-07-29 19:38:02 -0700

    Updated to Webpack 1.10.5. Removed loaders.

--- package.js
+++ package.js
@@ -1,21 +1,13 @@
 Package.describe({
   name: 'rocket:webpack',
-  version: '1.9.10_5',
+  version: '1.10.5',
   summary: 'Provides Webpack from NPM.',
   git: 'https://github.com/trusktr/rocket-webpack.git',
   documentation: 'README.md'
 })
 
 Npm.depends({
-    'webpack': "1.9.10",
-
-    // packages like rocket:module have to discover the location of the
-    // rocket:webpack isopack in the filesystem and tell Webpack to look in the
-    // isopack's `npm/node_modules` so that Webpack will be able to find the
-    // loaders.
-    'babel-loader': '5.0.0',
-    'css-loader': '0.12.0',
-    'style-loader': '0.12.1'
+    'webpack': "1.10.5"
 })
 
 Package.onUse(function(api) {
